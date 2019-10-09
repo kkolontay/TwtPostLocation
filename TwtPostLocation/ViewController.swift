b509edb64ea5e4f05e2ecd7ac004716d9160fffa
Konstantin Kolontay <kkolontay@gmail.com>
2019-10-09 12:44:57 -0400

    init

---
+++ TwtPostLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwtPostLocation
+//
+//  Created by kkolontay on 10/9/19.
+//  Copyright © 2019 com.kkolontay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
